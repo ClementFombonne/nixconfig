9444c14abe46531d96ad8d97defefa53fb9220d4
clement <clement.fombonne@proton.me>
2025-08-02 18:32:26 +0200

    update git conf

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,8 +1,13 @@
 { config, lib, pkgs, ... }:
 {
   programs.git = {
-      enable = true;
-      userName = "clement fombonne";
-      userEmail = "clement.fombonne@proton.me";
+enable = true;
+  userName = "clement fombonne";
+  userEmail = "clement.fombonne@proton.me";
+  extraConfig = {
+    init.defaultBranch = "main";
+    core.editor = "vim";
+    push.autoSetupRemote = true; 
+    };
   };
 }

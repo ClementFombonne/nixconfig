dfb3b7e488c299ad5efa18e033cee344299f8614
clement <clement.fombonne@proton.me>
2025-08-02 12:29:33 +0200

    restructure modules

--- flake.nix
+++ flake.nix
@@ -36,43 +36,24 @@
 
       modules = lib.flatten [
         ./hosts/${hostname}/configuration.nix
-	./modules/default.nix
-        (lib.optional bundles.hyprland ./modules/hyprland/bundle.nix)
-        (lib.optional bundles.app ./modules/app/bundle.nix)
-        (lib.optional bundles.dev ./modules/dev/bundle.nix)
-        (lib.optional bundles.terminal ./modules/terminal/bundle.nix)
+	./modules/nixos/default.nix
+        (lib.optional bundles.hyprland ./modules/nixos/hyprland/bundle.nix)
+        (lib.optional bundles.app ./modules/nixos/app/bundle.nix)
+        (lib.optional bundles.dev ./modules/nixos/dev/bundle.nix)
+        (lib.optional bundles.terminal ./modules/nixos/terminal/bundle.nix)
+	home-manager.nixosModules.home-manager
+        {
+	  home-manager.useGlobalPkgs = true;
+          home-manager.specialArgs = {inherit inputs;};
+	  home-manager.users.${user} = import ./hosts/${hostname}/home.nix;
+        }
       ];
-    };
-
-    makeHome = { hostname, stateVersion, bundles }: home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.${system};
-      specialArgs = {
-        inherit inputs hostname stateVersion user bundles;
-      };
-      modules = lib.flatten [
-        ./hosts/${hostname}/home.nix
-        ./home-manager/default.nix
-        (lib.optional bundles.hyprland ./home-manager/hyprland/bundle.nix)
-        # (lib.optional bundles.app ./modules/app/bundle.nix)
-        # (lib.optional bundles.dev ./modules/dev/bundle.nix)
-        # (lib.optional bundles.terminal ./modules/terminal/bundle.nix)
-      ];
-      home = {
-        username = user;
-	homeDirectory = "/home/${user}";
-      };
     };
   in {
     # Define NixOS configurations for each host
     nixosConfigurations = nixpkgs.lib.foldl' (configs: host:
       configs // {
         "${host.hostname}" = makeSystem {
-          inherit (host) hostname stateVersion bundles;
-        };
-      }) {} hosts;
-    homeConfigurations = nixpkgs.lib.foldl' (configs: host:
-      configs // {
-        "${user}" = makeHome {
           inherit (host) hostname stateVersion bundles;
         };
       }) {} hosts;
